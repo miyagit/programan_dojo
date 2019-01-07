9729c322efa7f368400071660dee799785f31a30
荒木真 <araki@arakimakotonoMacBook-Pro.local>
2019-01-07 16:33:46 +0900

    edit form_for

--- app/controllers/Main_controller.rb
+++ app/controllers/Main_controller.rb
@@ -1,4 +1,5 @@
 class MainController < ApplicationController
 	def top
+		@title = "form_forで投稿フォームを作ってみよう"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 	root 'main#top'
+	resources :main
   resources :users
 end
