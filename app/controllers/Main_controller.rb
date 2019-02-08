da71f53001d560b72053ec49450aaa2115419e39
荒木真 <araki@arakimakotonoMacBook-Pro.local>
2019-02-08 14:04:03 +0900

    first commit

--- app/controllers/Main_controller.rb
+++ app/controllers/Main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
 	def top
+		@title = "paramsを確認してみよう"
+		@users = User.all
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+		@jobs = Job.all
+	end
+
+	def create
+		User.create(user_params)
+		redirect_to root_path
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :job_id, :sex, :age, :tall ,:weight)
+	end
+
 end
