1aa2fcff7b15bfb6e214de657a7d4df166cb96df
Yuya Miyajima <yuya@MacBook-Pro.local>
2019-09-29 20:25:23 +0900

    setting data

--- app/controllers/Main_controller.rb
+++ app/controllers/Main_controller.rb
@@ -1,5 +1,6 @@
 class MainController < ApplicationController
   def top
     @title = 'allメソッドをどの様にビューで活用できるか確認しよう'
+    @users = User.all
   end
 end
--- db/migrate/20181128113500_create_users.rb
+++ db/migrate/20181128113500_create_users.rb
@@ -1,14 +1,18 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :email,             null: false, default: ""
       t.string           :name,    null: false, default: ""
       t.references       :job,     null: false, foreign_key: true
-      t.integer          :sex,     null: false
+      t.string           :sex,     null: false
       t.unsigned_integer :age,     null: false
-      t.unsigned_float   :tall,    null: false
-      t.unsigned_float   :weight,  null: false
+      t.unsigned_integer :tall,    null: false
+      t.unsigned_integer :weight,  null: false
+      t.unsigned_integer :income,  null: false
       t.timestamps
       t.timestamps
     end
+    add_index :users, :email,                unique: true
   end
 end
+
