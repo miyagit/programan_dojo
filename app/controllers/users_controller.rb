c162d5827c6c46c44a9e28c013a06d2829b25a4e
Yuya Miyajima <yuya@MacBook-Pro.local>
2019-10-28 12:28:38 +0900

    create_form implement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    binding.pry
+    if @user.save
+      redirect_to root_path, notice: 'userを作成できました。'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :job_id, :sex, :age, :tall, :weight, :income)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   has_many :circle_users
   has_many :circles, through: :circle_users
   belongs_to :job
+
+  validates :name, :age, :sex, :tall, :weight, :income, presence: true
+
+  def hello
+    "こんにちは、#{name}です。"
+  end
 end
